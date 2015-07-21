c989b088aeeb52ed515acf08b31523374b6fd743
d22shen <d22shen@hotmail.com>
2015-07-20 21:45:42 -0700

    test

--- PalinedromeLinkedList.cpp
+++ PalinedromeLinkedList.cpp
@@ -1,10 +1,18 @@
+//The structure of the list node
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x) : val(x), next(nullptr) {}
- };
-
+//my solution
+//two helper functions: get the lengeth of a list and reverse a list
+//separate the list into two halves
+//reverse the second half
+//compare the two half list
+//if all the same return true
+//else return false
+//time complexity O(n) and space complexity O(1)
 class Solution {
 public:
     
@@ -68,6 +76,8 @@ public:
             lasthalf = lasthalf->next;
         }
         
+        //reverse the previously reversed second half
+        //add the two halves together so that the list is not modified
         lasthalf = reverseList(lasthalf);
         
         if(temp == nullptr){
@@ -80,3 +90,108 @@ public:
         return true;
     }
 };
+
+//Reverse the first half and compare it with the second half
+//solution online
+//http://blog.csdn.net/abc123man/article/details/46835287
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next)//null or only 1 node
+        {
+            return true;
+        }
+        
+        ListNode *pre = NULL, *ne = NULL;//pre指向已经反转好的头节点
+        ListNode *phalf = head, *p = head;
+        bool ovenNode = false;//判断节点数是偶数还是奇数
+        while(p->next) //找出中间节点，并将前一半节点翻转
+        {
+            ne = phalf;
+            phalf = phalf->next;
+            p = p->next;
+            if(p->next)//p移动两次，phalf移动一次
+            {
+                p = p->next;
+            }
+            else
+            {
+                ovenNode = true;
+            }
+            
+            if(ne == head)//第一次不翻转
+            {
+                head->next = NULL;
+                pre = head;
+            }
+            else
+            {
+                ne->next = pre; //翻转
+                pre = ne;
+            }
+            
+        }
+        
+        if(!ovenNode) //奇数个节点，跳出中间节点不比较
+        {
+            phalf = phalf->next;
+        }
+        
+        while(phalf) //回文检测
+        {
+            if(pre->val != phalf->val)
+            {
+                return false;
+            }
+            phalf = phalf->next;
+            pre = pre->next;
+        }
+        
+        return true;
+    }
+};
+
+//use a vector to store the value
+//time complexity O(n) and space complexity O(n)
+//solution online
+//http://www.2cto.com/kf/201507/418110.html
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> temp;
+        ListNode* ptr = head;
+        while(ptr!=NULL)
+        {
+            temp.push_back(ptr->val);
+            ptr = ptr->next;
+        }
+        int n = temp.size();
+        for(int i = 0; i < n/2; i++)
+        {
+            if(temp[i] != temp[n-1-i])
+                return false;
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
